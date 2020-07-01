0d8a2b08dae2ed5d19aba331b47b2859af75e5c7
Oyeleke Okiki <oyelekeokiki@yahoo.com>
2020-07-01 20:20:01 +0100

    Dependency Injection setup

---
+++ app/src/main/java/com/oyelekeokiki/di/NetworkingModule.kt
@@ -0,0 +1,67 @@
+package com.oyelekeokiki.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.oyelekeokiki.networking.RemoteApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object NetworkingModule {
+
+  private const val HEADER_AUTHORIZATION = "Authorization"
+  private const val BASE_URL = "https://taskie-rw.herokuapp.com"
+  private const val AUTH_KEY = "https://taskie-rw.herokuapp.com"
+
+  @Provides
+  fun provideFactory(): Converter.Factory {
+    val contentType = "application/json".toMediaType()
+
+    return Json.nonstrict.asConverterFactory(contentType)
+  }
+
+  @Provides
+  fun provideAuthorizationInterceptor() = object : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+      val originalRequest = chain.request()
+      val new = originalRequest.newBuilder()
+        .addHeader(HEADER_AUTHORIZATION, AUTH_KEY)
+        .build()
+
+      return chain.proceed(new)
+    }
+  }
+
+  @Provides
+  fun provideClient(interceptor: Interceptor): OkHttpClient =
+    OkHttpClient.Builder()
+      .addInterceptor(HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+      })
+      .addInterceptor(interceptor)
+      .build()
+
+
+  @Provides
+  fun buildRetrofit(client: OkHttpClient, factory: Converter.Factory): Retrofit {
+    return Retrofit.Builder()
+      .client(client)
+      .baseUrl(BASE_URL)
+      .addConverterFactory(factory)
+      .build()
+  }
+
+  @Provides
+  fun buildApiService(retrofit: Retrofit): RemoteApiService =
+    retrofit.create(RemoteApiService::class.java)
+}
