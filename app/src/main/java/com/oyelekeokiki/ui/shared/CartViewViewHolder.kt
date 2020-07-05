0f0a864df7821d31fb9436a7f639ea6751f63d82
Oyeleke Okiki <oyelekeokiki@yahoo.com>
2020-07-05 01:44:25 +0100

    WIP Delete from cart with holder

--- app/src/main/java/com/oyelekeokiki/ui/shared/CartViewViewHolder.kt
+++ app/src/main/java/com/oyelekeokiki/ui/shared/CartViewViewHolder.kt
@@ -3,16 +3,27 @@ package com.oyelekeokiki.ui.shared
 import android.annotation.SuppressLint
 import android.view.View
 import com.oyelekeokiki.model.Product
+import kotlinx.android.synthetic.main.recycler_list_item_product.view.*
 
 @SuppressLint("SetTextI18n")
 class CartViewViewHolder(override val containerView: View) : ProductViewHolder(containerView) {
     override fun bindData(
         product: Product,
-        onWishListModified: onWishListModified,
-        onOnCartModified: onCartModified?,
+        onWishListModified: OnWishListModified?,
+        onCartModified: OnCartModified?,
         productIsLiked: Boolean
     ) {
-        super.bindData(product, onWishListModified, onOnCartModified, productIsLiked)
+        super.bindData(product, onWishListModified, onCartModified, productIsLiked)
+        containerView.add_to_cart_button.visibility = View.GONE
+        containerView.add_to_wishlist_button.visibility = View.GONE
+        containerView.remove_from_cart_button.visibility = View.VISIBLE
+
+        containerView.remove_from_cart_button.setOnClickListener {
+            onCartModified?.let {
+                it(product.id.toString())
+            }
+        }
+
     }
 
 }
