7743381c476af0ef207a2b62c55eebca8b6c5444
Oyeleke Okiki <oyelekeokiki@yahoo.com>
2020-07-07 02:10:54 +0100

    Implemented HomeViewModel tests

---
+++ app/src/androidTest/java/com/oyelekeokiki/HomeViewModelTest.kt
@@ -0,0 +1,89 @@
+package com.oyelekeokiki
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.oyelekeokiki.database.AppDataBase
+import com.oyelekeokiki.database.WishListDao
+import com.oyelekeokiki.database.WishListDatabaseSource
+import com.oyelekeokiki.helpers.NO_INTERNET_CONNECTION
+import com.oyelekeokiki.model.Product
+import com.oyelekeokiki.networking.RemoteApi
+import com.oyelekeokiki.ui.home.HomeViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.junit.MockitoJUnitRunner
+import java.io.IOException
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+class HomeViewModelTest {
+
+    @get:Rule
+    val testInstantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
+
+    private var appDataBase: AppDataBase? = null
+
+    @Mock
+    private lateinit var remoteApi: RemoteApi
+    @Mock
+    private lateinit var databaseSource: WishListDatabaseSource
+
+    @Mock
+    private lateinit var apiUsersObserver: Observer<List<Product>>
+
+    @Mock
+    private lateinit var errorMessageObserver: Observer<String>
+
+    @Before
+    fun setUp() {
+        databaseSource = WishListDatabaseSource(mock(WishListDao::class.java))
+    }
+
+    @Test
+    fun givenServerResponse200_whenFetch_shouldReturnSuccess() {
+        runBlocking {
+            val expectedResult = arrayListOf<Product>()
+            doReturn(expectedResult)
+                .`when`(remoteApi)
+                .getProducts()
+            val viewModel = HomeViewModel(remoteApi, databaseSource)
+            viewModel.products.observeForever(apiUsersObserver)
+            verify(remoteApi).getProducts()
+
+            verify(apiUsersObserver).onChanged(
+                expectedResult
+            )
+            viewModel.products.removeObserver(apiUsersObserver)
+        }
+    }
+
+    @Test
+    fun givenServerResponseError_whenFetch_shouldReturnError() {
+        runBlocking {
+            doThrow(RuntimeException(NO_INTERNET_CONNECTION))
+                .`when`(remoteApi)
+                .getProducts()
+            val viewModel = HomeViewModel(remoteApi, databaseSource)
+            viewModel.errorMessage.observeForever(errorMessageObserver)
+            verify(remoteApi).getProducts()
+            verify(errorMessageObserver).onChanged(
+                NO_INTERNET_CONNECTION
+            )
+            viewModel.errorMessage.removeObserver(errorMessageObserver)
+        }
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun tearDown() {
+        appDataBase?.close()
+    }
+}
